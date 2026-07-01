d2ad0774e8f5328f1aa3660eca25ca21ff920eb8
tracyngucs <tracyngucs@gmail.com>
2026-07-02 00:51:56 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "tilt-dodge-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./gamefile.html",
+  "./manifest.json",
+  "./icon.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
